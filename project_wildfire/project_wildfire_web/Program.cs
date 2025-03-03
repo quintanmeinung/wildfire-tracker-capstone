1f6530ebc364090596da1cfcb48e722278a8e6ba
YTMini <minitech@YTMinis-Mac-mini.local>
2025-03-02 18:29:29 -0800

    registered httpclient as a service

--- project_wildfire/project_wildfire_web/Program.cs
+++ project_wildfire/project_wildfire_web/Program.cs
@@ -52,6 +52,7 @@ public class Program
         builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
         builder.Services.AddScoped<IUserRepository, UserRepository>();
         builder.Services.AddScoped<IWildfireRepository, WildfireRepository>();
+        builder.Services.AddHttpClient();
 
 
         //adding swagger
