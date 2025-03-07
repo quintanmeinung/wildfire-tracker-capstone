dd66b8e8178989ac8e8fd64b2a535ad9878f314b
frekkdekk <mdeladurantaye20@mail.wou.edu>
2025-03-07 13:32:07 -0800

    Refactored WildfireApiController with proper API integration

---
+++ project_wildfire/project_wildfire_web/Services/NasaService.cs
@@ -0,0 +1,21 @@
+using project_wildfire_web.Models; 
+
+namespace project_wildfire_web.Services;
+
+public class NasaService : INasaService
+{
+    readonly HttpClient _httpClient;
+    readonly ILogger <NasaService> _logger;
+
+    public NasaService(HttpClient httpClient, ILogger<NasaService> logger)
+    {
+        _httpClient = httpClient;
+        _logger = logger;
+    }
+
+    // Hits /search/movie?={movie name} endpoint for movie search
+    public async Task<List<Fire>> GetFiresAsync()
+    {
+        throw new NotImplementedException();   
+    }
+}
