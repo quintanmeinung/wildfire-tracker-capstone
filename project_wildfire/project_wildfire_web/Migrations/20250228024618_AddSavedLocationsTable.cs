38a9785d02ef1c69bc454b6d1dc1ef63249c7c9f
quintanmeinung <quintanmeinung@gmail.com>
2025-03-07 17:25:41 -0800

    All User Preference Files should be working now: ~Program.cs has the right repository linked ~Both I and UserPreference Controllers/Repositories are updated ~It's now connected with the DbContext Database and cs files ~Register.cshtml.cs File is updated with User Preferences ~Migration files for UserPreferences FK are included

---
+++ project_wildfire/project_wildfire_web/Migrations/20250228024618_AddSavedLocationsTable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace project_wildfire_web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSavedLocationsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(name:"UserId", table: "UserPreferences");
+            migrationBuilder.AddForeignKey(name:"FK_UserPreferences_Users", table: "UserPreferences", column:"UserId", principalTable: "Users", principalColumn: "UserId", onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(name:"UserId", table:"UserPreferences");
+            migrationBuilder.AddPrimaryKey(name:"UserId", table: "UserPreferences", column:"UserId");
+        }
+    }
+}
