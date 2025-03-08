e2bff5f5f8650ad5402198ec45412bd0afa37e40
frekkdekk <mdeladurantaye20@mail.wou.edu>
2025-03-07 16:13:58 -0800

    Resolved issues with profile modal

--- project_wildfire/project_wildfire_web/Models/ProfileViewModel.cs
+++ project_wildfire/project_wildfire_web/Models/ProfileViewModel.cs
@@ -1,11 +1,17 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace project_wildfire_web.Models;
 public class ProfileViewModel
 {
+    public required string Id { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public required string? Email { get; set; }
     public string? PhoneNumber { get; set; }
     public string? Location { get; set; }
-    public required ICollection<UserLocation> SavedLocations { get; set; }
+
+    // SavedLocations has its own table, so it will need to be populated elsewhere
+    public ICollection<UserLocation> SavedLocations { get; set; } = [];
     public required ICollection<Fire> FireSubscriptions { get; set; }
+
 }
