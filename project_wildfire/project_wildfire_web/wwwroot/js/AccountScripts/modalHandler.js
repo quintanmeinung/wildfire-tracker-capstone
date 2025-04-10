0061993b989a4665c75e0a3b866b013cd601f4a3
Jonathan Reynoso <you@example.com>
2025-04-10 10:58:38 -0700

    can now log in to account settings.

--- project_wildfire/project_wildfire_web/wwwroot/js/AccountScripts/modalHandler.js
+++ project_wildfire/project_wildfire_web/wwwroot/js/AccountScripts/modalHandler.js
@@ -1,20 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Get the profile link
     var profileLink = document.getElementById("manage");
-    console.log("profileLink:", profileLink); // Debug profile link
 
     // Get the profile modal
-    var modalElement = document.getElementById('profileModal');
-    console.log("modalElement:", modalElement); // Debug modal element
-
-    var profileModal = new bootstrap.Modal(modalElement);
-    console.log("Bootstrap modal initialized");
+    var profileModal = new bootstrap.Modal(document.getElementById('profileModal'));
 
     // Add click event listener to the profile link
     if (profileLink) {
         profileLink.addEventListener("click", function (e) {
             e.preventDefault(); // Prevent default link behavior
-            console.log("Profile link clicked");
             profileModal.show(); // Show the modal
         });
     }
