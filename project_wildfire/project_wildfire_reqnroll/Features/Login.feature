9fca0a077df5833d31f0871a325fe8e3053a149e
frekkdekk <mdeladurantaye20@mail.wou.edu>
2025-04-15 18:02:54 -0700

    Removed vestigial files and added a LoginPageModel

---
+++ project_wildfire/project_wildfire_reqnroll/Features/Login.feature
@@ -0,0 +1,15 @@
+Feature: Login
+
+User's who visit the site can use their credentials to log in to their account.
+
+@login
+Scenario: successful login with valid credentials
+	Given I navigate to the login form
+	When I enter valid credentials
+	Then I should be logged in
+
+@login	
+Scenario: unsuccessful login with invalid credentials
+	Given I navigate to the login form
+	When I enter invalid credentials
+	Then I should see an error message
---
+++ project_wildfire/project_wildfire_reqnroll/StepDefinitions/LoginStepDefinitions.cs
@@ -0,0 +1,58 @@
+using System;
+using Reqnroll;
+using Selenium.WebDriver;
+
+namespace project_wildfire_reqnroll.StepDefinitions;
+
+[Binding]
+public sealed class LoginStepDefinitions
+{
+    
+    private readonly IWebDriver _driver;
+    /*
+    private readonly LoginPage _loginPage;
+ 
+    public LoginStepDefinitions(IWebDriver driver)
+    {
+        _driver = driver;
+        _loginPage = new LoginPage(_driver);
+    } */
+
+    [Given(@"I navigate to the login form")]
+    public void GivenINavigateToTheLoginForm()
+    {
+        //_loginPage.NavigateToLoginForm();
+        Console.WriteLine("Navigating to login form...");
+    }
+
+    [When(@"I enter valid credentials")]
+    public void WhenIEnterValidCredentials()
+    {
+        /* _loginPage.EnterCredentials("validUsername", "validPassword");
+        _loginPage.SubmitLoginForm(); */
+        Console.WriteLine("Entering valid credentials...");
+    }
+
+    [When(@"I enter invalid credentials")]
+    public void WhenIEnterInvalidCredentials()
+    {
+        /* _loginPage.EnterCredentials("invalidUsername", "invalidPassword");
+        _loginPage.SubmitLoginForm(); */
+        Console.WriteLine("Entering invalid credentials...");
+    }
+
+    [Then(@"I should be logged in")]
+    public void ThenIShouldBeLoggedIn()
+    {
+        Console.WriteLine("Checking if logged in...");
+        /* Assert.IsTrue(_loginPage.IsDashboardDisplayed(), "Dashboard is not displayed after login."); */
+    }
+
+    [Then(@"I should see an error message")]
+    public void ThenIShouldSeeAnErrorMessage()
+    {
+        Console.WriteLine("Checking for error message...");
+        /* Assert.IsTrue(_loginPage.IsDashboardDisplayed(), "Dashboard is not displayed after login."); */
+    }
+
+}
