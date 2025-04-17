da686816fb369c7444442b6b253051a7e806d3d6
frekkdekk <mdeladurantaye20@mail.wou.edu>
2025-04-17 10:54:35 -0700

    Added UserLocation PK; dynamic markers now persists to the DB

--- project_wildfire/project_wildfire_web/Models/UserLocation.cs
+++ project_wildfire/project_wildfire_web/Models/UserLocation.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace project_wildfire_web.Models;
 
 public partial class UserLocation
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public string UserId { get; set; } = null!;
 
