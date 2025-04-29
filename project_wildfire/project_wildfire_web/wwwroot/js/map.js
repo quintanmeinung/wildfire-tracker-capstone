0ed2cbeb07b5b8c98142e1dc0046f10f7274f595
quintanmeinung <quintanmeinung@gmail.com>
2025-04-28 18:15:50 -0700

    Quick fix for map.js to appear in deployment.

--- project_wildfire/project_wildfire_web/wwwroot/js/map.js
+++ project_wildfire/project_wildfire_web/wwwroot/js/map.js
@@ -112,8 +112,8 @@ document.addEventListener("DOMContentLoaded", function () {
             .finally(() => {
                 hideSpinner();
             });
-    });
-}
+    })
+
 
 // Spinner functions
 function showSpinner() {
